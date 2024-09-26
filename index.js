6527eaa3fb8debd7ac7d958dddc5971653638c86
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-26 03:15:14 -0700

    More http and node modules worked on

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const generateRandomNumber = require('./utils');
+import getPosts, { getPostsLength } from './postController.js';
 
-console.log(`Random Number : ${generateRandomNumber()}`);
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostsLength()}`);
+
+// const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to fahrenhiet: ${celciusToFahrenheit(0)}`);
